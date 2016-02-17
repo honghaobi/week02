31cc0d31370434f15524d4cd7cd88c0d6bc8f8e9
henrybi <henrybi@u.washington.edu>
2016-02-16 22:46:59 -0800

    finished javascript testing assignment

---
+++ javascript_fundamentals_part1/basic-js/script.js
@@ -0,0 +1,82 @@
+module.exports = {
+  sum: function(a,b){
+    return a+b;
+  },
+
+  isEqual: function(arg1,arg2){
+    if (arg1 === arg2){
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  isEven: function(num){
+    if (num % 2 === 0){
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  isDivisible: function(num1, num2){
+    if (num1 % num2 === 0){
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  discountPercentage: function(total, discount){
+    if (discount > 100 || discount < 0){
+      return "please enter a number between 0 and 100";
+    } else {
+      return total*(discount/100);
+    }
+  },
+
+  isVowel: function(letter){
+    if (letter === 'a' || letter === 'e' || letter === 'i' || letter === 'o' || letter === 'u'){
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  celsiusToFahrenheit: function(c2f, temp){
+    if (c2f){
+      return Math.floor((temp * 1.8) + 32);
+    } else {
+      return Math.floor((temp - 32) * (5/9));
+    }
+  },
+
+  biggestOfThree: function(a,b,c){
+    var array = [a, b, c];
+    var big = 0;
+    array.reduce(function(pre, cur){
+      if(cur > pre) {
+        big = cur;
+      }
+    });
+    return big;
+  },
+  // Bonus
+  federalIncomeTaxCalculator: function(income){
+    if (income >= 1 && income <= 9225 ){
+      return "you are paying $" + (income - (income * 0.1)) + " in tax";
+    } else if (income >= 9226 && income <= 37450){
+      return "you are paying $" + (income - (income * 0.15)) + " in tax";
+    } else if (income >= 37451 && income <= 90750){
+      return "you are paying $" + (income - (income * 0.25)) + " in tax";
+    } else if (income >= 90751 && income <= 189300){
+      return "you are paying $" + (income - (income * 0.28)) + " in tax";
+    } else if (income >= 189301 && income <= 411500){
+      return "you are paying $" + (income - (income * 0.33)) + " in tax";
+    } else if (income >= 411501 && income <= 413200){
+      return "you are paying $" + (income - (income * 0.15)) + " in tax";
+    } else if (income >= 413200){
+      return "you are paying $" + (income - (income * 0.396)) + " in tax";
+    }
+  }
+};
---
+++ javascript_fundamentals_part1/basic-js/test/jsFundamentalsSpec.js
@@ -0,0 +1,94 @@
+var expect = require("chai").expect; //jshint ignore:line
+var methods = require("../script");
+
+describe("#sum", function(){
+  it("adds two numbers", function(){
+    expect(methods.sum(1,2)).to.equal(3);
+  });
+  it("works with negative numbers", function(){
+    expect(methods.sum(-1,2)).to.equal(1);
+  });
+});
+
+describe("#isEqual", function(){
+  it("compares numbers", function(){
+    expect(methods.isEqual(1,2)).to.equal(false);
+  });
+  it("compares strings", function(){
+    expect(methods.isEqual(-1,-1)).to.equal(true);
+  });
+  it("compares booleans", function(){
+    expect(methods.isEqual(true,true)).to.equal(true);
+  });
+  it("compares type and equality (using ===)", function(){
+    expect(methods.isEqual("1",1)).to.equal(false);
+  });
+});
+
+describe("#isEven", function(){
+  it("returns true if a number is even", function(){
+    expect(methods.isEven(2)).to.equal(true);
+  });
+  it("returns false if a number is not even", function(){
+    expect(methods.isEven(3)).to.equal(false);
+  });
+});
+
+describe("#isDivisible", function(){
+   it("returns true if a number is divisible by the second argument", function(){
+    expect(methods.isDivisible(2,2)).to.equal(true);
+  });
+  it("returns false if a number is not divisible by the second argument", function(){
+    expect(methods.isDivisible(3,2)).to.equal(false);
+  });
+});
+
+describe("#discountPercentage", function(){
+  it("returns the discount for an amount", function(){
+    expect(methods.discountPercentage(100,5)).to.equal(5);
+  });
+  it("returns a warning if the percentage is below 0", function(){
+    expect(methods.discountPercentage(3,-5)).to.equal("please enter a number between 0 and 100");
+  });
+  it("returns a warning if the percentage is above 100", function(){
+    expect(methods.discountPercentage(3,102)).to.equal("please enter a number between 0 and 100");
+  });
+});
+
+describe("#isVowel", function(){
+  it("returns true if a letter is a vowel", function(){
+    expect(methods.isVowel("a")).to.equal(true);
+  });
+  it("returns false if a letter is not a vowel", function(){
+    expect(methods.isVowel("z")).to.equal(false);
+  });
+});
+
+describe("#celsiusToFahrenheit", function(){
+  it("returns Fahrenheit if its in Celsius", function(){
+    expect(methods.celsiusToFahrenheit(true,55)).to.equal(131);
+  });
+  it("returns Celsius if its in Fahrenheit", function(){
+    expect(methods.celsiusToFahrenheit(false,90)).to.equal(32);
+  });
+});
+
+describe("#biggestOfThree", function(){
+  it("returns biggest number of the three", function(){
+    expect(methods.biggestOfThree(25,928,73)).to.equal(928);
+  });
+  it("returns biggest number of the three", function(){
+    expect(methods.biggestOfThree(28,87333,9232)).to.equal(87333);
+  });
+});
+
+// BONUS - write a test for the bonus question!
+
+describe("#federalIncomeTaxCalculator", function(){
+  it("returns how much you are paying tax", function(){
+    expect(methods.federalIncomeTaxCalculator(86000)).to.equal("you are paying $64500 in tax");
+  });
+  it("returns how much you are paying tax", function(){
+    expect(methods.federalIncomeTaxCalculator(1000000)).to.equal("you are paying $604000 in tax");
+  });
+});
