5c22da40074af21121e3c99c103dec79aee6fb87
henrybi <henrybi@u.washington.edu>
2016-02-17 21:53:38 -0800

    finished assignment w/ all tests passed

--- javascript_fundamentals_part2/lecture.js
+++ javascript_fundamentals_part2/lecture.js
@@ -34,3 +34,44 @@ var openLockers = [];
   }
 
 console.log(openLockers);
+
+
+var players = [
+    {name: 'Bumgarner', number: '40', position: 'starting pitcher', salary: 6950000},
+    {name: 'Posey', number: '28', position: 'catcher', salary: 17277777},
+    {name: 'Belt', number: '9', position: 'first base', salary: 3600000},
+    {name: 'Panik', number: '12', position: 'second base', salary: 522500},
+    {name: 'Crawford', number: '35', position: 'shortstop', salary: 3175000},
+    {name: 'Duffy', number: '5', position: 'third base', salary: 509000},
+    {name: 'Aoki', number: '23', position: 'left field', salary: 4000000},
+    {name: 'Pagan', number: '16', position: 'center field', salary: 10250000},
+    {name: 'Pence', number: '8', position: 'right field', salary: 18500000},
+];
+
+console.log(players[2].number);
+console.log(players[6].position);
+console.log(players[1]);
+
+var totalSal= 0;
+
+for(var i = 0; i < players.length; i++) {
+    console.log(players[i].salary);
+    totalSal += players[i].salary;
+}
+
+console.log(totalSal);
+
+// Optional Argument
+
+function power(base, exponentS) {
+  var exponent = exponentS || 2;
+  var result = 1;
+  for (var count = 0; count < exponent; count++)
+    result *= base;
+  return result;
+}
+
+console.log(power(4));
+// → 16
+console.log(power(4, 3));
+// → 64
