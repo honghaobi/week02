1c2c53487af5d7bc517e0f334a9ffd7ad817dbfd
henrybi <henrybi@u.washington.edu>
2016-02-18 08:31:20 -0800

    adding querying the dom methods

---
+++ js-dom-basics/main.js
@@ -0,0 +1,26 @@
+var firstParagraph = document.getElementById('main');
+console.log(firstParagraph);
+
+var getAllElements = document.getElementsByTagName('ul');
+console.log(getAllElements);
+
+var getSingleElement = document.getElementsByTagName('ul')[0];
+console.log(getSingleElement);
+
+var allListItems = document.getElementsByTagName('li');
+console.log(allListItems);
+
+var evenElements = document.getElementsByClassName('even');
+console.log(evenElements);
+
+var oddElements = document.getElementsByClassName('odd');
+console.log(oddElements);
+
+var firstEvenElement = document.getElementsByClassName('even')[0];
+console.log(firstEvenElement);
+
+var firstOddElement = document.getElementsByClassName('odd')[0];
+console.log(firstOddElement);
+
+var main = document.querySelector('#main');
+console.log(main);
