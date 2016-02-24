58e750b65262e8d372c358eccc9fe798a1064468
henrybi <henrybi@u.washington.edu>
2016-02-24 11:57:00 -0800

    implemented esLint

--- checkerboard-exercise/script.js
+++ checkerboard-exercise/script.js
@@ -1,92 +1,94 @@
-// Your JS goes here
+(function() {
+  'use strict';
 
-// to see each function please un-comment out the funciton call.
+  // Your JS goes here
+  // to see each function please un-comment out the funciton call.
 
-var body = document.getElementsByTagName('body');
+  var body = document.getElementsByTagName('body');
 
+  function blackRed() {
+    for (var i = 0; i < 63; i++) {
+      var checkerbox = document.createElement('div');
 
-function blackRed(){
-  for (var i = 0; i < 63; i++) {
-    var checkerbox = document.createElement('div');
+      checkerbox.id = i;
+      checkerbox.style.width = '11.1%';
+      checkerbox.style.height = '100px';
+      checkerbox.style.float = 'left';
+      checkerbox.style.backgroundColor = 'black';
+      if (checkerbox.id % 2 === 0) {
+        checkerbox.style.backgroundColor = 'red';
+      }
 
-    checkerbox.id = i;
-    checkerbox.style.width = "11.1%";
-    checkerbox.style.height = "100px";
-    checkerbox.style.float = "left";
-    checkerbox.style.backgroundColor = "black";
-    if (checkerbox.id % 2 === 0) {
-      checkerbox.style.backgroundColor = "red";
+      body[0].appendChild(checkerbox);
     }
-
-    body[0].appendChild(checkerbox);
   }
-}
 
-// blackRed();
+  // blackRed();
 
-function randomColor(){
-  for (var i = 0; i < 63; i++) {
-    var checkerbox = document.createElement('div');
-    var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+  function randomColor() {
+    for (var i = 0; i < 63; i++) {
+      var checkerbox = document.createElement('div');
+      var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
 
-    checkerbox.id = i;
-    checkerbox.style.width = "11.1%";
-    checkerbox.style.height = "100px";
-    checkerbox.style.float = "left";
-    checkerbox.style.backgroundColor = hue;
+      checkerbox.id = i;
+      checkerbox.style.width = '11.1%';
+      checkerbox.style.height = '100px';
+      checkerbox.style.float = 'left';
+      checkerbox.style.backgroundColor = hue;
 
-    body[0].appendChild(checkerbox);
-  }
-}
-
-// randomColor();
-
-function gradientColor(){
-  for (var i = 0; i < 63; i++) {
-    var checkerbox = document.createElement('div');
-    var colorChange1 = 'rgb(' + (166) + ',' + (Math.floor(255-i*3)) + ',' + (0) + ')';
-    var colorChange2 = 'rgb(' + (Math.floor(0+(i*4))) + ',' + (144) + ',' + (155) + ')';
-
-    checkerbox.id = i;
-    checkerbox.style.width = "11.1%";
-    checkerbox.style.height = "100px";
-    checkerbox.style.float = "left";
-    checkerbox.style.backgroundColor = colorChange1;
-    if (checkerbox.id % 2 === 0) {
-      checkerbox.style.backgroundColor = colorChange2;
+      body[0].appendChild(checkerbox);
     }
-    body[0].appendChild(checkerbox);
   }
-}
 
-// gradientColor();
+  // randomColor();
 
-var hue;
-var checkerbox;
+  function gradientColor() {
+    for (var i = 0; i < 63; i++) {
+      var checkerbox = document.createElement('div');
+      var colorChange1 = 'rgb(' + (166) + ',' + (Math.floor(255 - i * 3)) + ',' + (0) + ')';
+      var colorChange2 = 'rgb(' + (Math.floor(0 + (i * 4))) + ',' + (144) + ',' + (155) + ')';
 
-function flashingColor(){
-  removeElementsByClass();
-  for (var i = 0; i < 63; i++) {
-    checkerbox = document.createElement('div');
-    hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
-    checkerbox.id = i;
-    checkerbox.className = "box";
-    checkerbox.style.width = "11.1%";
-    checkerbox.style.height = "100px";
-    checkerbox.style.float = "left";
-    checkerbox.style.backgroundColor = hue;
-    body[0].appendChild(checkerbox);
+      checkerbox.id = i;
+      checkerbox.style.width = '11.1%';
+      checkerbox.style.height = '100px';
+      checkerbox.style.float = 'left';
+      checkerbox.style.backgroundColor = colorChange1;
+      if (checkerbox.id % 2 === 0) {
+        checkerbox.style.backgroundColor = colorChange2;
+      }
+      body[0].appendChild(checkerbox);
+    }
   }
-}
 
-var element;
+  // gradientColor();
 
-function removeElementsByClass(){
+  var hue;
+  var checkerbox;
+
+  function flashingColor() {
+    removeElementsByClass();
+    for (var i = 0; i < 63; i++) {
+      checkerbox = document.createElement('div');
+      hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+      checkerbox.id = i;
+      checkerbox.className = 'box';
+      checkerbox.style.width = '11.1%';
+      checkerbox.style.height = '100px';
+      checkerbox.style.float = 'left';
+      checkerbox.style.backgroundColor = hue;
+      body[0].appendChild(checkerbox);
+    }
+  }
+
+  var element;
+
+  function removeElementsByClass() {
     var elements = document.getElementsByClassName('box');
-    while(elements.length > 0){
-        elements[0].parentNode.removeChild(elements[0]);
+    while (elements.length > 0) {
+      elements[0].parentNode.removeChild(elements[0]);
     }
-}
+  }
 
+  setInterval(flashingColor, 500);
 
-setInterval(flashingColor, 2000);
+})();
